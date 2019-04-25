566a50f090a72ad3533897fe71df2d296d18830e
CelestialCrafter <celestialcraftermc@gmail.com>
2019-04-25 05:04:18 -0400

    Update cuddle.js

--- commands/cuddle.js
+++ commands/cuddle.js
@@ -24,6 +24,6 @@ exports.conf = {
 exports.help = {
   name: 'cuddle',
   category: 'Weeb',
-  description: 'Returns a GIF of a hug.',
+  description: 'Returns a cuddle GIF.',
   usage: 'cuddle <user>'
 };
