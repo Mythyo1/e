131853500a1d503223a88c537bd6b6d9e03c1426
Rexogamer <beartechtalks@gmail.com>
2019-05-28 11:58:36 +0100

    Update bet.js

--- commands/bet.js
+++ commands/bet.js
@@ -2,27 +2,26 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    let settings = client.getSettings(message.guild.id);    
     let member = message.mentions.members.first();
     let embed;
     
     if (member) {
       if (Math.random() < 0.5) {
         embed = new Discord.RichEmbed()
-        .setTitle(message.author.username + ' betes ' + member.user.username)
+        .setTitle(message.author.username + ' bets on ' + member.user.username)
         .setColor('#363942')
-        .setDescription(message.author.username + ' bet ' + member.user.username + '!\n' + message.author.username + ' didn\'t win the bet!');
+        .setDescription(message.author.username + ' bet on ' + member.user.username + '!\n' + message.author.username + ' didn\'t win the bet!');
       } else {
         embed = new Discord.RichEmbed()
-        .setTitle(message.author.username + ' bet ' + member.user.username)
+        .setTitle(message.author.username + ' bets on ' + member.user.username)
         .setColor('#363942')
-        .setDescription(message.author.username + ' bet ' + member.user.username + ' and won the bet!');
+        .setDescription(message.author.username + ' bet on ' + member.user.username + ' and won the bet!');
       }
-    } else message.reply('You need to mention the person you want to bet!');
+    } else message.reply('You need to mention the person you want to bet on!');
     
     message.channel.send(embed);
   } catch (err) {
-    message.channel.send('Their was an error!\n' + err).catch();
+    message.channel.send('There was an error!\n' + err).catch();
   }
 };
 
