b57a70d8ecf9d69aed7393287631bc7ab5a6eb93
Rexogamer <beartechtalks@gmail.com>
2019-05-28 11:17:35 +0100

    Update mchead.js

--- commands/mchead.js
+++ commands/mchead.js
@@ -3,10 +3,10 @@ const request = require('request');
 
 exports.run = async (client, message, args, level) => {
   try {
-    if (!args[0]) return message.reply('You need to input the UUID or Name of the Minecraft Java Edition Player!');
+    if (!args[0]) return message.reply('You need to input the UUID or username of the Minecraft Java Edition player!');
     
     request('https://cravatar.eu/head/' + encodeURIComponent(args[0]) + '/400.png', (req, res, png) => {
-      if (png == 'Invalid minecraft username or uuid.') return message.reply('You need to input a Valid Java UUID/Name');
+      if (png == 'Invalid minecraft username or uuid.') return message.reply('You need to input a valid Java UUID/username');
       message.channel.send(new Discord.Attachment('https://cravatar.eu/head/' + args[0] + '/200.png'));
     });
   } catch (err) {
@@ -16,7 +16,7 @@ exports.run = async (client, message, args, level) => {
 
 exports.conf = {
   enabled: true,
-  aliases: [],
+  aliases: ['mcuser', 'minehead', 'mineuser'],
   guildOnly: false,
   permLevel: 'User'
 };
@@ -24,6 +24,6 @@ exports.conf = {
 exports.help = {
   name: 'mchead',
   category: 'Fun',
-  description: 'Returns the head of the specified Minecraft Player (Java Edition Only)',
-  usage: 'mchead <name/UUID>'
+  description: 'Returns the head of the specified Minecraft player (Java Edition only).',
+  usage: 'mchead <username/UUID>'
 };
