f5a65679be77c6530950b0790d0f0f3c8a65c0fa
Rexogamer <beartechtalks@gmail.com>
2019-05-29 10:31:10 +0100

    Grammar (hopefully)

--- modules/commands.js
+++ modules/commands.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const util = require('util');
 
-//Define variubles
+//Define variables
 const promisify = util.promisify;
 const readdir = promisify(fs.readdir);
 
@@ -11,27 +11,27 @@ module.exports = (client) => {
   let i = 1;
 
   readdir(__dirname + '/../commands/', (err, files) => {
-    //If their is an error, Return the error
+    //If there is an error, return the error
     if (err) return client.logger.error(err);
 
     //For each file in the file array run this function
     files.forEach((file) => {
-      //If the file extention (.py, .js, .md) is not js, Ignore it
+      //If the file extension (.py, .js, .md) is not js, ignore it
       if (!file.endsWith('.js')) return;
 
-      //Make the "props" variuble the file object
+      //Make the "props" variable the file object
       let props = require(`../commands/${file}`);
 
-      //Split the file name from the file extention
+      //Split the file name from the file extension
       let commandName = file.split('.')[0];
 
       //Log that the command is loading
       client.logger.log(`Loading command: ${commandName}. Command ${i}`);
 
-      //Set the command name the file objects
+      //Set the command name and the file objects
       client.commands.set(commandName, props);
       props.conf.aliases.forEach((al) => {
-        //Set the aliases of the command the file objects
+        //Set the aliases of the command and file objects
         client.aliases.set(al, client.commands.get(commandName));
       });
 
