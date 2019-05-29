7f9f8124b09d4ef6887aa70e69c6020736313277
Rexogamer <beartechtalks@gmail.com>
2019-05-29 10:54:16 +0100

    Update unload.js

--- commands/unload.js
+++ commands/unload.js
@@ -3,19 +3,19 @@ exports.run = async (client, message, args, level) => {// eslint-disable-line no
     if (!args || args.length < 1) return message.reply('You must provide a command to unload!');
 
     let response = await client.unloadCommand(args[0]);
-    if (response) return message.reply(`Error Unloading: ${response}`);
+    if (response) return message.reply(`Error unloading: ${response}`);
 
     client.logger.log(`Unloading Command: ${args[0]}`);
     message.reply(`The command \`${args[0]}\` has been unloaded`);
   } catch (err) {
-    message.channel.send('Their was an error!\n' + err).catch();
+    message.channel.send('There was an error!\n' + err).catch();
   }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['uload'],
   permLevel: 'Bot Moderator'
 };
 
