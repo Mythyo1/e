fba9bf31b5dbf7fd5e6169a1f19c8eb44cfc9ea5
Rexogamer <beartechtalks@gmail.com>
2019-06-04 18:58:31 +0100

    grammar

--- commands/sendfile.js
+++ commands/sendfile.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    if (!args[0]) return message.channel.send('You have to input the file to send');
+    if (!args[0]) return message.channel.send('You have to tell me which file to send!');
     
     message.author.send(new Discord.Attachment(fs.createReadStream('/app/' + args.join(' ')))).catch(() => {
-      return message.channel.send('I couldnt find the file or there was another error!');
+      return message.channel.send('I couldn\'t find the file or there was an error!');
     });
 
     
-    message.channel.send('The file is in your DM\'s!');
+    message.channel.send('The file has been sent to your DM\'s!');
   } catch (err) {
     message.channel.send('There was an error!\n' + err).catch();
   }
@@ -18,7 +18,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 
 exports.conf = {
   enabled: true,
-  aliases: ['sfl'],
+  aliases: ['sfl', 'sfile'],
   guildOnly: false,
   permLevel: 'Bot Manager'
 };
