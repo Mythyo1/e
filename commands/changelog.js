6ee334047d370692da46560a44fda542985d183e
CelestialCrafter <celestialcraftermc@gmail.com>
2019-07-20 17:22:35 -0400

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,6 +1,8 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-   let log = 'V2.6:\n' + 'Added two commands: toycat and sync (get the link for Toycat\'s channel and, for bot mods, sync the bot instance with the Github repo).\n' + 'More grammar fixes (thanks Rexo and EDGE!)\n' 'Coming soon: removal of some unused comamnds and more flippin\' grammar!';
+    //LEAVE EMPTY
+    //USE CHANGELOG.JSON
+   let log = '';
    require('../changelog').forEach((change) => log += '\n- ' + change);
   
    message.channel.send(log);
